73607801c2e7229e0024d8eedc9171388900304b
GeralmyUrena <geralmyuri11@gmail.com>
2022-12-24 10:39:51 -0500

    commit tree d4a538c5a0ebdeb2447d5e559165341998a209e0

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function createEmployeeRecord([string,string2,string3,number]){
+    //console.log(string)
+
+    let employeeRecord = 
+    
+   {}
+employeeRecord.firstName = string
+employeeRecord.familyName = string2
+employeeRecord.title = string3
+employeeRecord.payPerHour = number
+employeeRecord.timeInEvents =[]
+employeeRecord.timeOutEvents =[]
+
+return employeeRecord
+
+
+}
+
+function createEmployeeRecords(arrayNested){
+    
+    // Converts each nested `Array` into an employee record using
+    // `createEmployeeRecord` and accumulates it to a new `Array`
+if (Array.isArray(arrayNested)){
+    for (const array of arrayNested){
+
+        createEmployeeRecords(array)
+        createEmployeeRecord(array)
+        employeeRecords.push(createEmployeeRecord)
+        let employeeRecords = {}
+
+    }
+}
+    
+}
+
+
